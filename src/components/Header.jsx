a166052cebe99ed49990cc53e5d6804e931f3cfe
Sid <sid@Sids-MacBook-Pro.local>
2023-07-14 20:04:17 +0530

    started work on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../css/header.css'
 
 function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className='header flex'>
+      <div className="left-header-nav header-nav flex">
+        <div className="nav-link">Collections</div>
+        <div className="nav-link">Commission</div>
+      </div>
+      <div id="logo">Earhart</div>
+      <div className="right-header-nav header-nav flex">
+        <div className="nav-link">Boutiques</div>
+        <div className="nav-link">Appointments</div>
+      </div>
+    </div>
   )
 }
 
