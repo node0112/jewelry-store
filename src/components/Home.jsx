3c5182eb43ce82b202c1644ea09672bbc15c15a3
Sid <sid@Sids-MacBook-Pro.local>
2023-07-15 17:29:46 +0530

    added basic header layout and search animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function    Home() {
   return (
     <div className='home-container'>
         <div className="vid-bg"> 
-            <video src={bgVideo} autoPlay='true' loop='true' muted='true' style={{width: '100vw', height: '100vh', objectFit: 'cover'}}/>
+            <video src={bgVideo} autoPlay={true} loop={true} muted={true} style={{width: '100vw', height: '100vh', objectFit: 'cover'}}/>
             <div className="overlay"></div>
         </div>
     </div>
